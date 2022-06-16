fc2fc82df964f6538973d7a2a11527e078c38b88
Www-520 <15976999471@163.com>
2022-06-16 18:36:08 +0800

    补充更改

--- vite.nw.build.ts
+++ vite.nw.build.ts
@@ -1,4 +1,6 @@
+import { promisify } from 'util';
 import { parse } from 'url';
+import { readFileSync, writeFileSync } from 'fs';
 import { exec } from 'child_process';
 import {
     type InlineConfig,
@@ -13,7 +15,7 @@ import { main } from './package.json';
 
 interface Env {
     TARGET: 'dev' | 'prod' | undefined
-    RENDER: 'ssr' | 'csr' | undefined
+    PLATFORM: 'win-x86' | undefined
 }
 
 const env = process
@@ -25,12 +27,14 @@ const env = process
 }, {}) as Env;
 
 const DEFAULT_TARGET = 'prod';
-const { TARGET = DEFAULT_TARGET } = env;
+const DEFAULT_PLATFORM = 'win-x86';
+const { TARGET = DEFAULT_TARGET, PLATFORM = DEFAULT_PLATFORM } = env;
 const IS_PROD = TARGET === 'prod';
 const DEV_PORT = Number(parse(main).port);
 
 console.log(`当前传入的环境变量: ${JSON.stringify(env, null, 4)}`);
 console.log(`输出目标 ${TARGET}`);
+console.log(`输出平台 ${PLATFORM}`);
 IS_PROD || console.log(`监听端口 ${DEV_PORT}`);
 
 const config: InlineConfig = {
@@ -59,8 +63,16 @@ const config: InlineConfig = {
     ],
 };
 
+// 应该动态生成一致的名称, 自定义命名, 同时读取package.json的此配置来读取要操作的文件夹
+const dir = 'nw_svelte_ts_vite-1.0.0-win-x86';
+const file = `dist/${dir}/package.json`;
+const oldField = `"main":"${main}"`;
+const newField = '"main":"index.html"';
+
 IS_PROD
-    ? build(config)
+    ? promisify(exec)(`build --tasks ${PLATFORM} .`)
+        .then(() => writeFileSync(file, readFileSync(file).toString('utf-8').replace(oldField, newField)))
+        .then(() => build(config))
     : createServer(config)
         .then(server => server.listen(DEV_PORT))
-        .then(() => exec('nw .'));
+        .then(() => exec('run .'));
